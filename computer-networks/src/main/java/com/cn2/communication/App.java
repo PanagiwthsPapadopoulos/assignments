a531df28890ee9f27b55786cd25afe6788852d67
PanagiwthsPapadopoulos <ppapadopoulos.pantherauth@gmail.com>
2024-12-06 14:09:20 +0200

    final fixes and start of documentation

--- computer-networks/src/main/java/com/cn2/communication/App.java
+++ computer-networks/src/main/java/com/cn2/communication/App.java
@@ -8,7 +8,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
-import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 
@@ -35,24 +34,19 @@ public class App extends Frame implements WindowListener, ActionListener {
 	// TODO: Please define and initialize your variables here...
 
 	// Declare the receiver's IP and port as class-level variables
-	static String receiverAddressString = "192.168.2.9";
-	static String ownAddressString = "192.168.2.9";
-	static int receiverPort = 12346;
-	static int ownPort = 12345;
+	static String receiverAddressString = "ipAddress";	// change with correct address
+	static String ownAddressString = "ipAdress";		// change with correct address
+	static int receiverPort = 12346;					// change with correct port number
+	static int ownPort = 12345;							// change with correct port number
 	static public boolean outgoingCall = false;
 	static public boolean incomingCall = false;
 	static boolean activeCall = false;
 	static boolean activeTest = false;
 	
-	
-	// For testing the microphone
-	
-	
 
-    // Εκκίνηση καταγραφής και αναπαραγωγής σε διαφορετικά νήματα   
+    // Local Recorder and Player initialization for microphone testing
     private static AudioRecorder audioRecorderLocal;
     private static AudioPlayer audioPlayerLocal;
-    private static SinePlayer sinePlayerLocal;
     
     
     // End microphone testing
@@ -61,7 +55,7 @@ public class App extends Frame implements WindowListener, ActionListener {
 	 * Construct the app's frame and initialize important parameters
 	 */
 	public App(String title) {
-
+		
 		/*
 		 * 1. Defining the components of the GUI
 		 */
@@ -104,15 +98,8 @@ public class App extends Frame implements WindowListener, ActionListener {
 		sendButton.addActionListener(this);
 		callButton.addActionListener(this);
 		inputTextField.addActionListener(this);
-		testMicButton.addActionListener(this);
-
-		// Microphone testing
-		
-		
+		testMicButton.addActionListener(this); 
 	    
-	    
-		
-		
 	}
 
 	/**
@@ -131,21 +118,14 @@ public class App extends Frame implements WindowListener, ActionListener {
 
 		// String serverAddress = "127.0.0.1"; // Replace with the server's IP address
 		// int port = 5000;
-
+		receiverAddressString = "192.168.2.9";
 		/*
 		 * 2.
 		 */
 		try {
 			
-
 			// Create a DatagramSocket to listen on the specified port
 			DatagramSocket socket = new DatagramSocket(ownPort);
-
-			// Buffer to hold incoming data
-			byte[] buffer = new byte[1024];
-
-			// packet
-			DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
 
 			// Create the receiver
 			Receiver receiver = new Receiver(socket);
@@ -273,10 +253,6 @@ public class App extends Frame implements WindowListener, ActionListener {
 			}
 
 		} else if (e.getSource() == callButton) {
-
-			// The "Call" button was clicked
-
-			// TODO: Your code goes here...
 			try {
 				DatagramSocket socket = new DatagramSocket(); // Creates a socket for sending
 				Sender sender = new Sender(socket, InetAddress.getByName(receiverAddressString), receiverPort);
@@ -284,57 +260,41 @@ public class App extends Frame implements WindowListener, ActionListener {
 				// First outgoing Call
 				if (!outgoingCall && !incomingCall && !activeCall) {
 					// Start the client for initiating a call
-					System.out.println("Starting audio call as a client...");
-
 					sender.sendConnectionMessage("call");
-					// Send the appropriate message on the other end
+					
+					// Locally handle outgoing call
 					outgoingCall = true;
-
 					textArea.append("You started a call." + newline);
 
-					// Answer incoming call
+				// Answer incoming call
 				} else if (incomingCall) {
-
-					// Start the server for receiving a call
-					System.out.println("Waiting for an incoming audio call...");
-
-					sender.sendConnectionMessage("answer");
 					// Send the appropriate message on the other end
+					sender.sendConnectionMessage("answer");
+					
+					// Locally handle incoming call
 					incomingCall = false;
 					activeCall = true;
-
 					textArea.append("You accepted the call." + newline);
 
-				} else if (outgoingCall) {
-					// Cancel call
-
-					// End Call
+				// End Call
 				} else if (activeCall) {
-
-					System.out.println("Ending active audio call...");
-
+					// Send the appropriate message on the other end
 					sender.sendConnectionMessage("end-call");
 
+					// Locally handle cancelling the call
 					activeCall = false;
 					outgoingCall = false;
 					incomingCall = false;
-
 					textArea.append("You ended the call." + newline);
 				}
-
 				socket.close(); // Close the socket
 
 			} catch (Exception exception) {
 				exception.printStackTrace();
 			}
-
 			
-
 		} else if (e.getSource() == testMicButton) {
-			
-			
-
-		    // Open/close microphone
+			// Open/close microphone
 			activeTest = !activeTest;
 			System.out.println("Mic status: " + activeTest);
 			
@@ -346,8 +306,7 @@ public class App extends Frame implements WindowListener, ActionListener {
 				textArea.append("Microphone Active" + newline);
 			} else {
 				textArea.append("Microphone Inactive" + newline);
-			}
-				
+			}	
 		}
 		
 		buttonLogic();
